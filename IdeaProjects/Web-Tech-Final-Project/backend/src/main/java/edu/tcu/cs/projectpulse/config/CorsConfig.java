7620b670f9f9db55952ef9d88f0ca6427eaa0b0f
Diego Alejandro Vega Burbara <diegoalejandrovegaburbara@MacBook-Pro-DiegoV.local>
2026-05-04 01:20:11 -0500

    Fixed backend

--- IdeaProjects/Web-Tech-Final-Project/backend/src/main/java/edu/tcu/cs/projectpulse/config/CorsConfig.java
+++ IdeaProjects/Web-Tech-Final-Project/backend/src/main/java/edu/tcu/cs/projectpulse/config/CorsConfig.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.cors.allowed-origins:http://localhost:5173}")
+    @Value("${app.cors.allowed-origins:*}")
     private String allowedOrigins;
 
     @Bean
